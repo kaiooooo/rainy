4ff859481c790dd33f4cf246c5f353a1a713804b
ᴸᴼx <kaitozhiroki@gmail.com>
2022-09-05 17:42:29 -0300

    javascript
    .

---
+++ type-boolean.js
@@ -0,0 +1,9 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero); // <---- comparação
+
+const texto1 = "alura";
+const texto2 = "boa";
+
+console.log(texto1 === texto2);
---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo";
+const texto2 = "Olá, mundo";
+const senha = "leflabruka";
+const stringDeNumeros = "468912";
+
+const citacao = "Meu nome é ";
+const MeuNome = "Kaio";
+
+//concatenação (+)
+
+console.log(citacao + MeuNome);
+
+//template string/literal
