48559a39023f7666a5b14b3af8835a1886fa639c
ᴸᴼx <seu-email>
2023-07-27 17:51:51 -0300

    new html, css and javascript.
    .

---
+++ script.js
@@ -0,0 +1,38 @@
+var musica = document.querySelector("audio");
+
+document.querySelector(".botao-play").addEventListener("click", tocarMusica);
+document.querySelector(".botao-pause").addEventListener("click", pausarMusica);
+
+musica.addEventListener("timeupdate", atualizarBarra);
+
+function tocarMusica() {
+  musica.play();
+  document.querySelector(".botao-pause").style.display = "block";
+  document.querySelector(".botao-play").style.display = "none";
+}
+
+function pausarMusica() {
+  musica.pause();
+  document.querySelector(".botao-pause").style.display = "none";
+  document.querySelector(".botao-play").style.display = "block";
+}
+
+function atualizarBarra() {
+  var barra = document.querySelector("progress");
+  barra.style.width =
+    Math.floor((musica.currentTime / musica.duration) * 100) + "%";
+  var tempoDecorrido = document.querySelector(".inicio");
+  tempoDecorrido.textContent = segundosParaMinutos(
+    Math.floor(musica.currentTime)
+  );
+}
+
+function segundosParaMinutos(segundos) {
+  var campoMinuto = Math.floor(segundos / 60);
+  let campoSegundos = segundos % 60;
+  if (campoSegundos < 10) {
+    campoSegundos = "0" + campoSegundos;
+  }
+
+  return campoMinuto + ":" + campoSegundos;
+}
